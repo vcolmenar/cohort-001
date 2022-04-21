7fc6bfd9d5e5d39c5f1f1f4a6d2abd7383e90567
Vincent Colmenar <vcolmenar@gmail.com>
2022-04-21 18:50:09 -0400

    added typescript, added plain button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React from 'react';
-
+import React from "react";
 
 const App = () => {
   return (
     <div>
-      Hello World!
+      <button>MyButton</button>
     </div>
   );
-}
+};
 
 export default App;
